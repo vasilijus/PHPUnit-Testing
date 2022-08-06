9671f9b3c71c1c01c2b533155ab208febb579976
Sergej V <vasiliokas@gmail.com>
2022-08-06 23:51:33 +0100

    Unit-1 email class

---
+++ 01-test/src/Email.php
@@ -0,0 +1,34 @@
+<?php 
+declare(strict_types=1);
+
+namespace UnitTest;
+
+use InvalidArgumentException;
+
+final class Email
+{
+    private $emai;
+
+    private function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public static function fromString(string $email): self
+    {
+        return new self($email);
+    }
+
+    private function enumIsValidEmail(string $email): void 
+    {
+        if( !filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+    
+}
